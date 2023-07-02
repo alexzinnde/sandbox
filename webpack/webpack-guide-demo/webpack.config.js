b3869d8b2b95606a44fddf1dbc30079747c503f3
Alexander Zinn <61348227+alexzinnde@users.noreply.github.com>
2023-07-02 14:41:22 -0400

    asset management

--- webpack/webpack-guide-demo/webpack.config.js
+++ webpack/webpack-guide-demo/webpack.config.js
@@ -3,8 +3,20 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
